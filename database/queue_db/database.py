345e28d2edd4fd1b900d846b9efa2ee19f0a2720
belomord <belomors@yandex.ru>
2025-01-20 05:09:12 +0300

    Подключение к БД

---
+++ database/queue_db/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from utils.cfg import cfg_db
+
+
+engine = create_engine(f'sqlite:///{cfg_db.QUEUE_DB_NAME}.sqlite')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+def create_db():
+    Base.metadata.create_all(engine)
+
